f17a5aee02097d7620f1f3c4afb723eeecebcd91
KEI <KEI@DESKTOP-I7ID0TL>
2020-09-06 23:21:22 +0700

    Package GameEngine by create team develop version 0

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #Example Create GUI Window
-from Bin.Gui import *
-import glfw
-#
-winGui = Gui(1200,600,'test',True,None)
-#
-while not winGui.gui_Close:
-    #
-    winGui.gui_update()
-#
-winGui.gui_terminate()
+# Import package name   (my package create)
+# GameGame Module name GUI
+from GameEngine import GUI
+win=GUI.Gui()
+# win2 = GUI.Gui()
+winApp=win.gui_create(400,300,"Test")
+# winApp1=win2.gui_create(400,300,"Test2")
+while not win.gui_Close:
+    win.gui_update()
+    # win2.gui_update()
+# win2.gui_terminate()
+win.gui_terminate()
+
