93a110d07ad23c8fa6e1e669f2d0ce0c535e94e8
KEI <KEI@DESKTOP-I7ID0TL>
2020-09-14 04:19:40 +0700

    Add module GUI System 0.0.1 function setFullsrceenMode v.0.0 and setWindowMode v0.0 setIcon v0.0 bug path icon

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-#Example Create GUI Window
+# Example Create GUI Window
 # Import package name   (my package create)
 # GameGame Module name GUI
+import sys
+
 from GameEngine import GUI
 win=GUI.Gui()
-# win2 = GUI.Gui()
+#
 winApp=win.gui_create(400,300,"Test")
-# winApp1=win2.gui_create(400,300,"Test2")
+
 while not win.gui_Close:
     win.gui_update()
-    # win2.gui_update()
-# win2.gui_terminate()
-win.gui_terminate()
 
+win.gui_terminate()
