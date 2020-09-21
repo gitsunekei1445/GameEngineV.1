d2788647506cb9a2716ef5780029426263c8ca6f
KEI <KEI@DESKTOP-I7ID0TL>
2020-09-21 22:34:38 +0700

    Develop gui_SetFullsrceenMode Competed Develop gui_SetWindowMode Competed Develop gui_SetTitle Competed Develop gui_SetIcon BugPath Develop gui_ShowFPS Competed

--- main.py
+++ main.py
@@ -2,13 +2,28 @@
 # Import package name   (my package create)
 # GameGame Module name GUI
 import sys
+from OpenGL.GL import glClear,GL_COLOR_BUFFER_BIT,glClearColor
+
 
 from GameEngine import GUI
+from GameEngine.Rendering import Render
+from OpenGL.GL import *
+
 win=GUI.Gui()
-#
-winApp=win.gui_create(400,300,"Test")
 
+winApp=win.gui_create(1270,800,"Test")
+# Render.set_background(0.5,0.5,0.5,1.0)
+# Render.draw_rect(0,0)
 while not win.gui_Close:
-    win.gui_update()
+    # glDisableClientState(GL_VERTEX_ARRAY)
+    # Render.render_update()
 
+    # glDrawArrays(GL_POLYGON, 0, 4)
+
+    win.gui_update()
+    win.gui_showFPS()
 win.gui_terminate()
+
+
+
+
